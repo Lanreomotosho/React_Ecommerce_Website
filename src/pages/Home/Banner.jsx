987ffe051de1cec4cc32971a6d0e10d4075a8165
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-05-11 23:59:24 +0100

    first commit

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import bannerImg from "../images/banner.png"
+import bannerImg from "/images/banner.png"
 
 export const Banner = () => {
   return (
     <div className='bg-primaryBG py-12 xl:px-28 px-4'>
 <div>
 <div className='md:w-1/2'>
-<h1 className='text-5'>
-
+<h1 className='text-5x1 font-light mb-5'>Collections
 </h1>
+
+<p className='text-xl mb-7'>
+you can explore ans shop many differnt collection
+from various barands here.
+</p>
+<button className=''>
+    Shop Now
+</button>
     </div>
 
     <div className='md:w-1/2'>
