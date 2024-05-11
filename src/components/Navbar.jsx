13fcdfee7f56f36a7e9ea38231779d33132d78e6
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-05-11 23:54:56 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,6 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
 
      const [isMenuOpen,  setIsMenuOpen] = useState(false);
-
      const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen)
      }
--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import bannerImg from "../images/banner.png"
 
 export const Banner = () => {
   return (
     <div className='bg-primaryBG py-12 xl:px-28 px-4'>
 <div>
-    <div className='md:w'></div>
+<div className='md:w-1/2'>
+<h1 className='text-5'>
+
+</h1>
+    </div>
+
+    <div className='md:w-1/2'>
+        <img src={bannerImg} alt='' />
+    </div>
+
 </div>
     </div>
   )
